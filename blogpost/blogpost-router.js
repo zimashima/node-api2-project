9c271516f0b7c56238a06da65399c48c8e537b8f
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-14 16:39:20 -0500

    POST and DELETE has been successfully created, tested and they both work, and return the correct messages

--- blogpost/blogpost-router.js
+++ blogpost/blogpost-router.js
@@ -34,12 +34,28 @@ router.get("/:id", (req, res) => {
 
 //POST a new post
 router.post("/", (req, res) => {
-    
+    if ((req.body.title === undefined) || (req.body.contents === undefined )){
+        res.status(400).json({ errorMessage: "Please provide TITLE and CONTENTS for the post." })
+    } else {
+        postDB.insert(req.body)
+            .then( newpost => res.status(201).json( newpost ))
+            .catch( error => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+    }
 })
 
 //DELETE a post
 router.delete("/:id", (req, res) => {
-    
+    const id = req.params.id
+    postDB.findById(id)
+        .then( post => {
+            if ( post.length === 0){
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                postDB.remove(id)
+                    .then( result => res.status(200).json({ message: `ID ${id} is successfully deleted`}))
+                    .catch( error => res.status(500).json({ error: "The post could not be removed" }))
+                }
+        })
 })
 
 //PUT a post
